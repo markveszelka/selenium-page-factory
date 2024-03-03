554f8d0f00c016810c2d981ec63b792022b8685d
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2024-03-03 19:49:45 +0100

    Add RadioButton class and its test class too

--- src/main/java/com/example/seleniumpomdesignpattern/page/RadioButton.java
+++ src/main/java/com/example/seleniumpomdesignpattern/page/RadioButton.java
@@ -1,13 +1,52 @@
 package com.example.seleniumpomdesignpattern.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class RadioButton extends InputForm {
+
+    public final static By RADIO_BUTTONS_MENU_ELEMENT = By.xpath("//*[@id=\"treemenu\"]/li/ul/li[1]/ul/li[3]/a");
+    public final static By MALE_1_ELEMENT = By.xpath("//input[@value='Male' and @name='optradio']");
+    public final static By FEMALE_1_ELEMENT = By.xpath("//input[@value='Female' and @name='optradio']");
+    public final static By GET_VALUE_BUTTON = By.id("buttoncheck");
+    public final static By GET_SINGLE_VALUE = By.xpath("//p[@class='radiobutton']");
+    public final static By MALE_2_ELEMENT = By.xpath("//input[@value='Male' and @name='gender']");
+    public final static By FEMALE_2_ELEMENT = By.xpath("//input[@value='Female' and @name='gender']");
+    public final static By ZERO_TO_FIVE = By.xpath("//input[@value='0 - 5']");
+    public final static By FIVE_TO_FIFTEEN = By.xpath("//input[@value='5 - 15']");
+    public final static By FIFTEEN_TO_FIFTY = By.xpath("//input[@value='15 - 50']");
+    public final static By GET_VALUES_BUTTON = By.xpath("//*[@id=\"easycont\"]/div/div[2]/div[2]/div[2]/button");
+    public final static By GET_MULTIPLE_VALUES = By.xpath("//p[@class='groupradiobutton']");
+
     public RadioButton(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void selectRadioButtons() {
+        driver.findElement(RADIO_BUTTONS_MENU_ELEMENT).click();
+        driver.findElement(GET_VALUE_BUTTON).click();
+    }
 
+    public void singleRadioButton(By genderRadioButton) {
+        driver.findElement(genderRadioButton).click();
+        driver.findElement(GET_VALUE_BUTTON).click();
+    }
+
+    public void multipleRadioButtons(List<By> radioButtons) {
+        for (By radioButton : radioButtons) {
+            driver.findElement(radioButton).click();
+        }
+        driver.findElement(GET_VALUES_BUTTON).click();
+    }
+
+    public String getSingleValue() {
+        return driver.findElement(GET_SINGLE_VALUE).getText();
+    }
+
+    public String getMultipleValues() {
+        return driver.findElement(GET_MULTIPLE_VALUES).getText();
     }
 }
---
+++ src/test/java/com/example/seleniumpomdesignpattern/page/RadioButtonTest.java
@@ -0,0 +1,84 @@
+package com.example.seleniumpomdesignpattern.page;
+
+import com.example.seleniumpomdesignpattern.config.WebDriverProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+class RadioButtonTest {
+
+    private static final String TEST_PAGE_URL = "https://web.archive.org/web/20180930221201/http://www.seleniumeasy.com/test/";
+    private WebDriver driver;
+    private SimpleFormDemo simpleFormDemo;
+
+    public static Stream<Arguments> singleRadioTestCases() {
+        return Stream.of(
+                Arguments.of(By.xpath("//input[@value='Male' and @name='optradio']")),
+                Arguments.of(By.xpath("//input[@value='Female' and @name='optradio']"))
+        );
+    }
+
+    public static Stream<Arguments> multipleRadioTestCases() {
+        return Stream.of(
+                Arguments.of(List.of(RadioButton.MALE_2_ELEMENT, RadioButton.FIFTEEN_TO_FIFTY)),
+                Arguments.of(List.of(RadioButton.FEMALE_2_ELEMENT, RadioButton.FIFTEEN_TO_FIFTY))
+        );
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = WebDriverProvider.setupWebDriver();
+        driver.get(TEST_PAGE_URL);
+        // Arrange
+        simpleFormDemo = new SimpleFormDemo(driver);
+        simpleFormDemo.navigateIntoInputFormsMenu();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @ParameterizedTest
+    @MethodSource("singleRadioTestCases")
+    @DisplayName("Test the single radio button is checked or not")
+    void singleRadioButton(By radioElement) {
+        // Arrange
+        RadioButton radioButton = new RadioButton(driver);
+        // Act
+        radioButton.selectRadioButtons();
+        radioButton.singleRadioButton(radioElement);
+        // Assert
+        String expected = String.format(
+                "Radio button '%s' is checked",
+                driver.findElement(radioElement).getAttribute("value"));
+        Assertions.assertEquals(expected, radioButton.getSingleValue());
+    }
+
+    @ParameterizedTest
+    @MethodSource("multipleRadioTestCases")
+    @DisplayName("Test the multiple radio buttons are checked or not")
+    void multipleRadioButtons(List<By> radioButtons) {
+        // Arrange
+        RadioButton radioButton = new RadioButton(driver);
+        // Act
+        radioButton.selectRadioButtons();
+        radioButton.multipleRadioButtons(radioButtons);
+        // Assert
+        String gender = driver.findElement(radioButtons.get(0)).getAttribute("value");
+        String age = driver.findElement(radioButtons.get(1)).getAttribute("value");
+        String expected = String.format("Sex : %s\n" +
+                "Age group: %s", gender, age);
+        String actual = radioButton.getMultipleValues();
+        Assertions.assertEquals(expected, actual);
+    }
+}
